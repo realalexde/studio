8d8e218cdda8179e77f4b06f6d266eda888a73c4
realalexde <realalexde.g@gmail.com>
2025-06-10 08:52:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarMenu class

--- src/app/(app)/visual-code/page.tsx
+++ src/app/(app)/visual-code/page.tsx
@@ -1,4 +1,20 @@
-import { CodeGenerator } from "@/components/visual-code/code-generator";
+
+"use client";
+
+import dynamic from 'next/dynamic';
+import { Skeleton } from "@/components/ui/skeleton";
+
+const CodeGenerator = dynamic(() =>
+  import("@/components/visual-code/code-generator").then(mod => mod.CodeGenerator),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="container mx-auto py-8">
+        <Skeleton className="w-full max-w-4xl h-[700px] mx-auto rounded-lg" />
+      </div>
+    )
+  }
+);
 
 export default function AIVisualCodePage() {
   return (
